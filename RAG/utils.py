877610fdf9e6f1321258d62eaa227b6e66eb2dba
tminh <nguyenhuutuanminhkh@gmail.com>
2024-11-29 23:43:53 +0700

    update some lines

--- RAG/utils.py
+++ RAG/utils.py
@@ -11,7 +11,12 @@ def load_reader_model(reader_model_name: str) -> Pipeline:
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
-    model = AutoModelForCausalLM.from_pretrained(reader_model_name, quantization_config=bnb_config)
+    model = AutoModelForCausalLM.from_pretrained(
+        reader_model_name, 
+        quantization_config=bnb_config,
+        device_map='auto',
+        trust_remote_code=True,)
+    
     tokenizer = AutoTokenizer.from_pretrained(reader_model_name)
 
     llm = pipeline(
