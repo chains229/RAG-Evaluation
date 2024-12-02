1e797e412227b2ba34650d49c96ae6b17bafc709
tminh <nguyenhuutuanminhkh@gmail.com>
2024-12-02 22:22:57 +0700

    old code was stupid af

--- database/utils.py
+++ database/utils.py
@@ -65,47 +65,6 @@ def chunk_documents(documents: str, chunk_size: int, chunk_overlap: int, tokeniz
     return docs_processed_unique
 
 
-def split_documents(chunk_size: int, chunk_overlap:int, 
-                    knowledge_base: List[LangchainDocument],
-                    tokenizer_name: Optional[str] = EMBEDDING_MODEL_NAME) -> List[LangchainDocument]:
-    """
-    Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
-    """
-    text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-        AutoTokenizer.from_pretrained(tokenizer_name),
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-        add_start_index=True,
-        strip_whitespace=True,
-        separators=NEWS_SEPARATORS,
-    )
-
-    docs_processed = []
-    for doc in knowledge_base:
-        docs_processed += text_splitter.split_documents([doc])
-
-    # Remove duplicates
-    unique_texts = {}
-    docs_processed_unique = []
-    for doc in docs_processed:
-        if doc.page_content not in unique_texts:
-            unique_texts[doc.page_content] = True
-            docs_processed_unique.append(doc)
-
-    return docs_processed_unique
-
-NEWS_SEPARATORS = [
-    "\n\n",   # Paragraphs
-    "\n",     # Line breaks
-    "\t",     # Tabs
-    ". ",     # Sentences
-    ", ",     # Clauses within sentences
-    "; ",     # Semi-colons separating clauses
-    ": ",     # Colons introducing lists or explanations
-    " ",      # Spaces
-    ""        # No space, as a last resort
-]
-
 # Function to read PDF files
 def read_pdf(file_path):
     reader = PdfReader(file_path)
